6ae9c97869c7d386fcac028f538e95a5737090a1
Neil Parley <mail@neilparley.co.uk>
2016-01-20 15:37:43 +0000

    For ubuntu us wget to download the deb file first. Then install using gdebi. Correct bugs in required lines. Move the service ouside of the case and add a user for the rstudio login.

--- manifests/packages/rstudio.pp
+++ manifests/packages/rstudio.pp
@@ -4,16 +4,17 @@ class datashield::packages::rstudio {
   case $::operatingsystem {
     'Ubuntu': {
 
-      package { 'gdebi-core':  ensure => 'installed', } ->
+      include gdebi
+      wget::fetch { 'https://download2.rstudio.org/rstudio-server-0.99.491-amd64.deb':
+        destination => '/tmp/rstudio-server-0.99.491-amd64.deb',
+        timeout     => 0,
+        verbose     => false,
+      } ->
       package { 'rstudio':
         ensure => 'installed',
-        source => 'https://download2.rstudio.org/rstudio-server-0.99.491-amd64.deb',
-        provider => 'deb',
-        require => '::r',
-      } ~>
-      service { 'rstudio-server':
-        ensure => running,
-        enable => true,
+        source => '/tmp/rstudio-server-0.99.491-amd64.deb',
+        provider => 'gdebi',
+        require => Class['::r'],
       }
 
     }
@@ -24,14 +25,24 @@ class datashield::packages::rstudio {
         ensure => 'installed',
         source => 'https://download2.rstudio.org/rstudio-server-rhel-0.99.491-x86_64.rpm',
         provider => 'rpm',
-        require => '::r',
-      } ~>
-      service { 'rstudio-server':
-        ensure => running,
-        enable => true,
+        require => Class['::r'],
       }
     }
 
   }
+
+  user { 'datashield':
+    ensure   => present,
+    password => 'mrtyHtvJlH8D2',
+    notify => Service['rstudio-server'],
+    managehome => true,
+  }
+
+  service { 'rstudio-server':
+    require => Package['rstudio'],
+    ensure => running,
+    enable => true,
+  }
+
 }
 
