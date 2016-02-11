0d50c19fb4d996c22a64d3a6c6f4bb890fe2e151
Neil Parley <mail@neilparley.co.uk>
2016-02-11 14:51:37 +0000

    Change to opal_url to reflect naming of similar variable in the Opal module

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,10 +1,10 @@
-define datashield::server($r_path = '/usr/bin/R', $opal_password = 'password', $url='http://localhost:8080') {
+define datashield::server($r_path = '/usr/bin/R', $opal_password = 'password', $opal_url='http://localhost:8080') {
 
   include ::r
 
   exec { "install_datashield_package_${name}":
-    command => "${r_path} -e \"library(opaladmin); o<-opal.login('administrator', '${opal_password}', url='${url}'); dsadmin.install_package(o, '${name}'); dsadmin.set_package_methods(o, pkg='${name}')\" | grep 'TRUE' ",
-    unless  => "${r_path} -e \"library(opaladmin); o<-opal.login('administrator', '${opal_password}', url='${url}'); dsadmin.installed_package(o, '${name}')\" | grep 'TRUE' ",
+    command => "${r_path} -e \"library(opaladmin); o<-opal.login('administrator', '${opal_password}', url='${opal_url}'); dsadmin.install_package(o, '${name}'); dsadmin.set_package_methods(o, pkg='${name}')\" | grep 'TRUE' ",
+    unless  => "${r_path} -e \"library(opaladmin); o<-opal.login('administrator', '${opal_password}', url='${opal_url}'); dsadmin.installed_package(o, '${name}')\" | grep 'TRUE' ",
     require => [Class['::r'], Class[::opal::install], ::R::Package['opaladmin']]
   }
 
